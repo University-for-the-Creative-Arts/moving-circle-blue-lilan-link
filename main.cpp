0b04b9710fe74442f0030ceb9f7cf77c0603034c
Sam <samanthaoblong@gmail.com>
2025-09-29 14:58:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,75 @@
+/*******************************************************************************************
+*
+*   raylib [core] example - Keyboard input
+*
+*   Example originally created with raylib 1.0, last time updated with raylib 1.0
+*
+*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
+*   BSD-like license that allows static linking with closed source software
+*
+*   Copyright (c) 2014-2024 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
 #include "raylib.h"
 
-int main() {
-    const int screenWidth = 960;
-    const int screenHeight = 600;
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "Raylib Starter Template");
-    SetTargetFPS(60);
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
 
-    while (!WindowShouldClose()) {
+    Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+    float ballRadius = 50.0f;
+    
+
+    void DrawHitbox(bool isColliding);
+
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+        //----------------------------------------------------------------------------------
+        //Ball Wall Collision
+        if (ballPosition.x + ballRadius > screenWidth) ballPosition.x = screenWidth - ballRadius;
+        if (ballPosition.x - ballRadius < 0) ballPosition.x = ballRadius;
+        if (ballPosition.y + ballRadius > screenHeight) ballPosition.y = screenHeight - ballRadius;
+        if (ballPosition.y - ballRadius < 0) ballPosition.y = ballRadius;
+        
+        // Draw
+        //----------------------------------------------------------------------------------
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+
+            DrawCircleV(ballPosition, 50, MAROON);
+
+
         EndDrawing();
+        //----------------------------------------------------------------------------------
     }
 
-    CloseWindow();
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
     return 0;
 }
